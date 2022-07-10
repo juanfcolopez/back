45e0404e131e9046d77d920aded06c8d018e8c38
juanfcolopez <jflopez2@uc.cl>
2022-07-10 13:19:44 -0400

    Add workers view endpoints

--- app.py
+++ app.py
@@ -2,8 +2,10 @@ from flask import Flask
 from flask_cors import CORS
 from routes.payments import payments_bp
 from routes.products import products_bp
+from routes.workers import workers_bp
 app = Flask(__name__)
 CORS(app)
 
 app.register_blueprint(payments_bp)
 app.register_blueprint(products_bp)
+app.register_blueprint(workers_bp)
